c409d7215154fea25e10336a1b932dde8e7bdf07
Prathamesh <prathameshetc1@gmail.com>
2020-08-25 12:56:23 +0530

    Initial commit

---
+++ src/test/java/loginTest.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class loginTest {
+public static WebDriver driver;
+@BeforeTest
+public void setUp() {
+	System.setProperty("webdriver.gecko.driver", "C:\\Users\\EPIQ\\NewWorkspace\\GitTest1\\src\\driver\\geckodriver.exe");
+	driver=new FirefoxDriver();
+	
+}
+@Test
+public void doLogin() {
+	driver.get("http://www.google.co.in");
+}
+
+@AfterTest
+public void tearDown() {
+	driver.quit();
+}
+}
